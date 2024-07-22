e645863acac16a03ce54105565f1da0f1e34fcd2
Nikita Solovyev <mr.utingen@gmail.com>
2024-07-22 13:18:58 +0300

    Сверстан экран профиля

---
+++ Image Feed/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  Image Feed
+//
+//  Created by Гена Утин on 22.07.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
