5f26b7c63add50d7ebac14b1109239de633ba008
Nikita Solovyev <mr.utingen@gmail.com>
2024-07-24 13:40:13 +0300

    Добавлен TabBar и ScrollView

---
+++ Image Feed/Profile/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  Image Feed
+//
+//  Created by Гена Утин on 22.07.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var AvatarImageView: UIImageView!
+    
+    @IBOutlet private var ExitButton: UIButton!
+    
+    @IBOutlet private var NameLabel: UILabel!
+    
+    @IBOutlet private var LoginLabel: UILabel!
+    
+    @IBOutlet private var DescriptionLabel: UILabel!
+    
+    @IBAction private func DidTapExitButton() { }
+}
