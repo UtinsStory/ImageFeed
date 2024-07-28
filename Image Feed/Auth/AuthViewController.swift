0b41e7e504de4b4eea0efb5471de04c88a059c82
Nikita Solovyev <mr.utingen@gmail.com>
2024-07-28 15:18:27 +0300

    Сверстан экран для веб-контента

--- Image Feed/Auth/AuthViewController.swift
+++ Image Feed/Auth/AuthViewController.swift
@@ -24,6 +24,7 @@ final class AuthViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureBackButton()
         
     }
     
@@ -40,7 +41,12 @@ final class AuthViewController: UIViewController {
     
     
     //MARK: - Methods
-    
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "nav_back_button")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back_button")
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named: "YP Black")
+    }
     
     
     
---
+++ Image Feed/Auth/WebViewViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WebViewViewController.swift
+//  Image Feed
+//
+//  Created by Гена Утин on 28.07.2024.
+//
+
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController {
+
+    private let showSWebViewIdentifier = "ShowWebView"
+    
+    @IBOutlet private var webView: WKWebView!
+    
+    
+    
+
+}
