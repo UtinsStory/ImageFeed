806b06c8242a4e2ce50fc1eb653476ac83724684
Nikita Solovyev <mr.utingen@gmail.com>
2024-07-28 14:55:31 +0300

    Сверстан экран авторизации

---
+++ Image Feed/Auth/AuthViewController.swift
@@ -0,0 +1,49 @@
+//
+//  AuthViewController.swift
+//  Image Feed
+//
+//  Created by Гена Утин on 28.07.2024.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+    //MARK: - IB Outlets
+    @IBOutlet private var screenLogoImageView: UIImageView!
+    
+    @IBOutlet private var loginButton: UIButton!
+    
+    
+    //MARK: - Properties
+    
+    
+    
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    
+    //MARK: IB Actions
+    @IBAction func didTapLoginButton() {
+        //TODO: Логика работы
+    }
+    
+    
+    
+    
+    
+    
+    //MARK: - Methods
+    
+    
+    
+    
+    
+    
+}
