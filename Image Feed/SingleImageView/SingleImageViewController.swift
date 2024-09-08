e0ab66b92590e07ca091b5a819440955b92a9909
Nikita Solovyev <mr.utingen@gmail.com>
2024-09-08 21:28:27 +0300

    добавлен полноэкранный просмотр фотографии

--- Image Feed/SingleImageView/SingleImageViewController.swift
+++ Image Feed/SingleImageView/SingleImageViewController.swift
@@ -36,13 +36,14 @@ final class SingleImageViewController: UIViewController {
     }
     
     
-    private func rescaleAndCenterImageInScrollView() {
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        imageView.bounds.size = image.size
         let minZoomScale = scrollView.minimumZoomScale
         let maxZoomScale = scrollView.maximumZoomScale
         view.layoutIfNeeded()
         
         let visibleRectSize = scrollView.bounds.size
-        let imageSize = imageView.frame.size
+        let imageSize = image.size
         let hScale = visibleRectSize.width / imageSize.width
         let vScale = visibleRectSize.height / imageSize.height
         let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
@@ -65,12 +66,26 @@ final class SingleImageViewController: UIViewController {
             guard let self = self else {return}
             switch result {
             case .success(let imageResult):
-                self.rescaleAndCenterImageInScrollView()
+                self.rescaleAndCenterImageInScrollView(image: imageResult.image)
             case .failure:
                 print("[SingleImageViewController]:[setImage]: Error getting single image")
+                self.showError(photo)
             }
         }
         imageView.frame.size = photo.size
+    }
+    
+    private func showError(_ photo: Photo) {
+        let alert = UIAlertController(title: "Что-то пошло не так.",
+                                      message: "Попробовать еще раз?",
+                                      preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Не надо", style: .cancel)
+        let retryAction = UIAlertAction(title: "Повторить", style: .default) { [weak self] _ in
+            self?.setImage(photo)
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(retryAction)
+        present(alert, animated: true)
     }
     
     override func viewDidLoad() {
