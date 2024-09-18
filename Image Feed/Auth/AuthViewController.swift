1b2eec49dc35c9aa75a3cf61d7e35a67c129bf93
Nikita Solovyev <mr.utingen@gmail.com>
2024-09-18 10:52:54 +0300

    Создан AuthHelper. Часть логики перенесена из WebViewPresenter в AuthHelper

--- Image Feed/Auth/AuthViewController.swift
+++ Image Feed/Auth/AuthViewController.swift
@@ -37,7 +37,8 @@ final class AuthViewController: UIViewController,
                 assertionFailure("Failed to prepare for \(showSWebViewIdentifier)")
                 return
             }
-            let webViewPresenter = WebViewPresenter()
+            let authHelper = AuthHelper()
+            let webViewPresenter = WebViewPresenter(authHelper: authHelper)
             webViewViewController.presenter = webViewPresenter
             webViewPresenter.view = webViewViewController
             webViewViewController.delegate = self
--- Image Feed/Auth/WebViewPresenter.swift
+++ Image Feed/Auth/WebViewPresenter.swift
@@ -16,30 +16,21 @@ public protocol WebViewPresenterProtocol {
 final class WebViewPresenter: WebViewPresenterProtocol {
     weak var view: WebViewControllerProtocol?
     
+    var authHelper: AuthHelperProtocol
+    
+    init(authHelper: AuthHelperProtocol) {
+        self.authHelper = authHelper
+    }
+    
     private enum WebViewConstants {
         static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
     }
     
     func viewDidload() {
-        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString)
-        else {
-            assertionFailure("Invalid authorization URL string: \(WebViewConstants.unsplashAuthorizeURLString)")
-            return
-        }
-        
-        urlComponents.queryItems  = [
-            URLQueryItem(name: "client_id", value: Constants.accessKey),
-            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
-            URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: Constants.accessScope)
-        ]
-        guard let url = urlComponents.url else {
-            assertionFailure("Failed to construct authorization URLRequest with components: \(urlComponents)")
-            return
-        }
-        let request = URLRequest(url: url)
-        didUpdateProgressValue(0)
+        guard let request = authHelper.authRequest() else { return }
+    
         view?.load(request: request)
+        didUpdateProgressValue(0)
     }
     
     func didUpdateProgressValue(_ newValue: Double) {
@@ -55,15 +46,6 @@ final class WebViewPresenter: WebViewPresenterProtocol {
     }
     
     func code (from url: URL) -> String? {
-        if
-            let urlComponents = URLComponents(string: url.absoluteString),
-            urlComponents.path == "/oauth/authorize/native",
-            let items = urlComponents.queryItems,
-            let codeItem = items.first(where: { $0.name == "code" })
-        {
-            return codeItem.value
-        } else {
-            return nil
-        }
+        authHelper.code(from: url)
     }
 }
